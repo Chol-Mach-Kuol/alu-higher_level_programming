7a350f4d6c84bbc40b5c14130b11295e90bf26a8
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-02-21 18:42:31 +0200

    Fix task 8: exactly 8 lines

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
+word_first = str[39:67]
+word_second = str[107:112]
+word_third = str[:6]
+str = word_first + word_second + word_third
 print(str)
-
