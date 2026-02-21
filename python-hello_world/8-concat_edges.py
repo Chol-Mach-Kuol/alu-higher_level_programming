3ca586fa98e350c8e85b33b1b41c7dcb593af5ab
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-02-21 18:47:42 +0200

    Fix all tasks: correct line counts and requirements

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-word_first = str[39:67]
-word_second = str[107:112]
-word_third = str[:6]
-str = word_first + word_second + word_third
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
