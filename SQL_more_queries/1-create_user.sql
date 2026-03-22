f4e339c9c5cb197e9ddcf15cf51238df00fa10ba
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-03-22 23:15:55 +0200

    Clean up merge conflicts in SQL files

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
--- Creates the MySQL server user user_0d_1
--- Grants all privileges and sets password to user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-=======
 -- Creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
->>>>>>> f4ac4969c5b6388e66bd1d29b708b0c5260cbb7c
