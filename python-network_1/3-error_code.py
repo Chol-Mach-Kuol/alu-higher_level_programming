9c320e317cf29d3da766a439a7aa0bd87e32c12e
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-03-22 17:50:18 +0200

    Add Python Network 1 project

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a request and handles HTTP errors using urllib"""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as r:
+            print(r.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
