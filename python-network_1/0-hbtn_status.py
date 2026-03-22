1656aa7bfb303621c88dc26b455db133d16e8add
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-03-22 18:15:44 +0200

    Fix task 0: only urllib, hardcoded URL, proper docstring

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
+"""Fetches https://alu-intranet.hbtn.io/status and displays the body response"""
 import urllib.request
-import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1] if len(sys.argv) > 1 else 'https://alu-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as r:
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as r:
         body = r.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
