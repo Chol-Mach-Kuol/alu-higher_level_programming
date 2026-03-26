7a801d2576e2a3f7c45505a7fca40702d6caa8e4
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-03-27 01:01:06 +0200

    Task 1: create both users as root; Task 2: create user_2_db and grant SELECT, INSERT to user_0d_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,5 @@
--- Create database hbtn_0d_2 and user user_0d_2 with SELECT and INSERT privileges
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create database user_2_db and user user_0d_2 with SELECT and INSERT privileges
+CREATE DATABASE IF NOT EXISTS user_2_db;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_2'@'localhost';
-GRANT SELECT, INSERT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
