24cb4ef6b76c4c208ebf84759661659f9429c500
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-03-27 00:59:06 +0200

    Task 2: revoke root privileges from user_0d_2 and grant SELECT, INSERT on user_2_db

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- Create database user_2_db and user user_0d_2 with SELECT and INSERT privileges
 CREATE DATABASE IF NOT EXISTS user_2_db;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_2'@'localhost';
 GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
