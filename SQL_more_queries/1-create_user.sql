a23f4b4ea9571b50c52d2c02a51d5aad39900485
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-03-27 01:00:06 +0200

    Task 1: only create user_0d_1; Task 2: create user_0d_2 with SELECT and INSERT on hbtn_0d_2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
--- Creates the MySQL server users user_0d_1 and user_0d_2.
+-- Creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
